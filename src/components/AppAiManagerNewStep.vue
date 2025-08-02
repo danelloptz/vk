19c254da1f29784fe38c47de803d5b31360149ed
danellotpz <denvasik05@gmail.com>
2025-08-02 15:53:29 +0300

    сделанная страница с добавлением шага

--- src/components/AppAiManagerNewStep.vue
+++ src/components/AppAiManagerNewStep.vue
@@ -11,7 +11,7 @@
         </div>
         <div class="row">
             <span>Текст</span>
-            <input v-model="name" type="text" />
+            <AppAiTextEditor />
         </div>
         <div class="row">
             <span>Изображения</span>
@@ -25,10 +25,10 @@
                 :class="{ 'dragover': isDragging }"
             >
                 <div class="icon">
-                    <svg viewBox="0 0 24 24" fill="" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path fill-rule="evenodd" clip-rule="evenodd" d="M10 1C9.73478 1 9.48043 1.10536 9.29289 1.29289L3.29289 7.29289C3.10536 7.48043 3 7.73478 3 8V20C3 21.6569 4.34315 23 6 23H7C7.55228 23 8 22.5523 8 22C8 21.4477 7.55228 21 7 21H6C5.44772 21 5 20.5523 5 20V9H10C10.5523 9 11 8.55228 11 8V3H18C18.5523 3 19 3.44772 19 4V9C19 9.55228 19.4477 10 20 10C20.5523 10 21 9.55228 21 9V4C21 2.34315 19.6569 1 18 1H10ZM9 7H6.41421L9 4.41421V7ZM14 15.5C14 14.1193 15.1193 13 16.5 13C17.8807 13 19 14.1193 19 15.5V16V17H20C21.1046 17 22 17.8954 22 19C22 20.1046 21.1046 21 20 21H13C11.8954 21 11 20.1046 11 19C11 17.8954 11.8954 17 13 17H14V16V15.5ZM16.5 11C14.142 11 12.2076 12.8136 12.0156 15.122C10.2825 15.5606 9 17.1305 9 19C9 21.2091 10.7909 23 13 23H20C22.2091 23 24 21.2091 24 19C24 17.1305 22.7175 15.5606 20.9844 15.122C20.7924 12.8136 18.858 11 16.5 11Z" fill=""></path> </g></svg>
+                    <img src="@/assets/images/upload.png" />
                 </div>
                 <div class="text">
-                    <span>Click to upload image</span>
+                    <span>Перетащите файлы сюда</span>
                 </div>
                 <input
                     id="file"
@@ -64,14 +64,26 @@
                         <li
                             v-for="(item, index) in types"
                             :key="index"
-                            @mousedown.prevent="selectType(item)"
+                            @mousedown.prevent="selectType(item, index)"
                             :class="{ bold: index == activeIndex }"
                         >
                             {{ item }}
                         </li>
                     </ul>
                 </div>
-                <input v-model="date" type="date" />
+                <div class="datetime-wrapper">
+                    <input
+                        ref="datetimeInput"
+                        v-model="date"
+                        type="datetime-local"
+                        class="datetime"
+                    />
+                    <img
+                        src="@/assets/images/time.png"
+                        class="calendar-icon"
+                        @click="openPicker"
+                    />
+                </div>
             </div>
         </div>
         <div class="new_send_footer">
@@ -84,9 +96,10 @@
 <script>
     import AppGoodButton from '@/components/AppGoodButton.vue';
     import AppBadButton from '@/components/AppBadButton.vue';
+    import AppAiTextEditor from '@/components/AppAiTextEditor.vue';
 
     export default {
-        components: { AppGoodButton, AppBadButton },
+        components: { AppGoodButton, AppBadButton, AppAiTextEditor },
         data() {
             return {
                 isDropdownVisible: false,
@@ -95,9 +108,20 @@
                 activeIndex: -1,
                 isDragging: false,
                 previews: [],
+                name: '',
+                title: '',
+                text: '',
             }
         },
         methods: {
+            openPicker() {
+                const input = this.$refs.datetimeInput;
+                if (input.showPicker) {
+                    input.showPicker();
+                } else {
+                    input.focus(); // fallback
+                }
+            },
             onDragOver() {
                 this.isDragging = true;
             },
@@ -133,8 +157,9 @@
             hideDropdown() {
                 this.isDropdownVisible = false;
             },
-            selectType(item) {
+            selectType(item, index) {
                 this.type = item;
+                this.activeIndex = index;
                 this.hideDropdown();
             },
         }
@@ -142,6 +167,49 @@
 </script>
 
 <style scoped>
+    .ck-editor__editable {
+        background-color: #1b1e3c;
+        color: white;
+        min-height: 150px;
+        border-radius: 8px;
+        padding: 10px;
+    }
+    .datetime-wrapper {
+        position: relative;
+        display: inline-block;
+        width: fit-content;
+    }
+
+    .datetime {
+        padding-left: 60px; /* чтобы не перекрылась иконкой */
+        color: white;
+        background: transparent;
+        border: 1px solid white;
+        font-size: 18px;
+        border-radius: 10px;
+    }
+
+    /* Убираем встроенную иконку календаря (в Chrome/Webkit) */
+    .datetime::-webkit-calendar-picker-indicator {
+        opacity: 0;
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        left: 0;
+        top: 0;
+        cursor: pointer;
+    }
+
+    /* Иконка поверх */
+    .calendar-icon {
+        position: absolute;
+        left: 20px; /* сдвинуть влево, если надо больше – увеличь */
+        top: 50%;
+        transform: translateY(-50%);
+        width: 20px;
+        height: 20px;
+        cursor: pointer;
+    }
     .preview-list {
         display: flex;
         gap: 10px;
@@ -181,11 +249,15 @@
         display: flex;
         align-items: center;
         justify-content: center;
+        width: 55px;
+        height: 55px;
+        border-radius: 50%;
+        background: #1B1E3D;
     }
 
-    .custum-file-upload .icon svg {
-        height: 80px;
-        fill: #e8e8e8;
+    .custum-file-upload .icon img {
+        width: 25px;
+        height: 25px;
     }
 
     .custum-file-upload .text {
@@ -195,8 +267,9 @@
     }
 
     .custum-file-upload .text span {
-        font-weight: 400;
-        color: #e8e8e8;
+        font-size: 18px;
+        font-family: 'OpenSans';
+        color: rgba(255, 255, 255, 0.3);
     }
 
     .custum-file-upload input {
@@ -309,7 +382,7 @@
         letter-spacing: 1px;
         scrollbar-width: none;
         z-index: 15;
-        padding: 20px;
+        padding: 0px 20px;
         @media (max-width: 650px) {
             font-size: 16px;
         }
@@ -325,12 +398,11 @@
 
     .dropdown-menu li {
         cursor: pointer;
-        border-bottom: 1px solid white;
+        border-bottom: 1px solid rgba(255, 255, 255, 0.3);
         font-family: 'OpenSans';
         padding: 10px 0px;
     }
-
-    .dropdown-menu li:hover {
-        background: #0c103e;
+    .dropdown-menu li:last-of-type {
+        border-bottom: none;
     }
 </style>
