6936bed1128d022bf95ac9e4e1a0c64e6022d423
danellotpz <denvasik05@gmail.com>
2025-08-13 22:45:36 +0300

    диалоги без сообщений

--- src/components/AppAiManagerDialog.vue
+++ src/components/AppAiManagerDialog.vue
@@ -37,6 +37,64 @@
                     <img src="@/assets/images/send_message.png" class="send_message" />
                 </div>
             </div>
+            <div class="dialog_info">
+                <div class="dialog_info_item" style="align-self: center; align-items: center;">
+                    <img :src="activeMan.img" class="dialog_info_item_img"/>
+                    <h2>{{ activeMan.name }}</h2>
+                </div>
+                <div class="dialog_info_item">
+                    <span class="mute">ID</span>
+                    <span>{{ activeMan.id }}</span>
+                </div>
+                <div class="dialog_info_item">
+                    <span class="mute">Телефон</span>
+                    <span>{{ activeMan.phone != "" ? activeMan.phone : '-' }}</span>
+                </div>
+                <div class="dialog_info_item">
+                    <span class="mute">Username</span>
+                    <span>{{ activeMan.username }}</span>
+                </div>
+                <div class="dialog_info_item">
+                    <span class="mute">Статус</span>
+                    <span>{{ activeMan.status }}</span>
+                </div>
+                <div class="dialog_info_item">
+                    <span class="mute">Первое сообщение</span>
+                    <span>{{ activeMan.first_msg }}</span>
+                </div>
+                <div class="dialog_info_item">
+                    <span class="mute">Первая активность</span>
+                    <span>{{ formatedFullDate(activeMan.fisrt_active) }}</span>
+                </div>
+                <div class="dialog_info_item">
+                    <span class="mute">Последняя активность</span>
+                    <span>{{ formatedFullDate(activeMan.last_active) }}</span>
+                </div>
+                <AppBadButton :text="'+ ДОБАВИТЬ ПОЛЕ'" class="add_field_btn" />
+                <div class="dialog_info_item">
+                    <span class="mute">Теги</span>
+                    <span
+                        v-for="(item, index) in activeMan.tags"
+                        :key="index"
+                        class="dialog_info_item_tag"
+                    >
+                        {{ item }}
+                        <img src="@/assets/images/close.png" class="delete_tag" @click="deleteTag(index, item)"/>
+                    </span>
+                    <AppBadButton :text="'+ ДОБАВИТЬ'" class="add_tag_btn" @click="openNewTags" />
+                    <div class="dropdown_tags" v-if="isNewTags">
+                        <div 
+                            v-for="(tag, tag_index) in activeMan.userTags.filter(tag => !activeMan.tags.includes(tag))"
+                            :key="tag_index"
+                            class="dropdown_tag"
+                            @click="addTag(tag)"
+                        >
+                            {{ tag }}
+                        </div>
+                    </div>
+                    <AppGoodButton :text="'СОХРАНИТЬ'" v-if="tagBuffer.length > 0" @click="saveTags" class="save_tags" />
+                </div>
+            </div>
         </div>
     </section>
 </template>
@@ -44,8 +102,9 @@
 
 <script>
     import AppGoodButton from '@/components/AppGoodButton.vue';
+    import AppBadButton from '@/components/AppBadButton.vue';
     export default {
-        components: { AppGoodButton },
+        components: { AppGoodButton, AppBadButton },
         data() {
             return {
                 people: [
@@ -105,10 +164,45 @@
                     last_active: 1755095394934,
                     tags: ["Работа"],
                     userTags: ["Книги", "Спорт", "Работа", "Продвижение", "Искусство"],
-                }
+                },
+                isNewTags: false,
+                tagBuffer: []
             }
         },
+        created() {
+            document.addEventListener('click', this.handleClickOutside);
+        },
         methods: {
+            handleClickOutside(event) {
+                const clickedEl = event.target;
+                if (!clickedEl.closest('.dropdown_tags') && !clickedEl.closest('.add_tag_btn') && this.isNewTags != -1) {
+                    this.closeNewTags();
+                }
+            },
+            saveTags() {
+                this.tagBuffer = [];
+                this.closeNewTags();
+            },
+            openNewTags() {
+                this.isNewTags = true;
+            },
+            closeNewTags() {
+                this.isNewTags = false;
+            },
+            addTag(tag) {
+                this.activeMan.tags.push(tag);
+
+                if (this.tagBuffer.indexOf(tag) == -1) this.tagBuffer.push(tag)
+                else this.tagBuffer = this.tagBuffer.filter(t => t != tag);
+
+                this.closeNewTags();
+            },
+            deleteTag(index, tag) {
+                this.activeMan.tags.splice(index, 1);
+
+                if (this.tagBuffer.indexOf(tag) == -1) this.tagBuffer.push(tag)
+                else this.tagBuffer = this.tagBuffer.filter(t => t != tag);
+            },
             formatedDate(time) {
                 const date = new Date(time);
 
@@ -118,11 +212,97 @@
 
                 return `${day}.${month}.${year}`;
             },
+            formatedFullDate(time) {
+                const date = new Date(time);
+
+                const day = String(date.getDate()).padStart(2, '0');
+                const month = String(date.getMonth() + 1).padStart(2, '0');
+                const year = date.getFullYear();
+                const hours = String(date.getHours()).padStart(2, '0');
+                const minutes = String(date.getMinutes()).padStart(2, '0');
+
+                return `${day}.${month}.${year} ${hours}:${minutes}`;
+            },
         }
     };
 </script>
 
 <style scoped>
+    .dropdown_tags {
+        position: absolute;
+        left: 110px;
+        top: 50px;
+        z-index: 999;
+        background: #434665;
+        padding: 10px 8px;
+        display: flex;
+        flex-direction: column;
+    }
+    .dropdown_tag {
+        font-size: 15px;
+        color: white;
+        font-family: 'OpenSans';
+        display: flex;
+        align-items: center;
+        padding: 8px 0px;
+        border-bottom: 1px solid rgba(255, 255, 255, 0.3);
+    }
+    .delete_tag {
+        width: 10px;
+        height: 10px;
+        cursor: pointer;
+    }
+    .dialog_info_item_tag {
+        background: #111433;
+        padding: 4px 10px;
+        border-radius: 5px;
+        font-size: 16px;
+        font-family: 'OpenSans';
+        color: white;
+        display: flex;
+        align-items: center;
+        column-gap: 10px;
+        width: fit-content;
+    }
+    .add_tag_btn {
+        width: 95px;
+        height: 32px;
+        font-size: 9.98px;
+        letter-spacing: 0px;
+    }
+    .add_field_btn, .save_tags {
+        width: 130px;
+        height: 32px;
+        font-size: 9.98px;
+        letter-spacing: 0px;
+    }
+    .mute {
+        color: rgba(255, 255, 255, 0.5) !important;
+    }
+    .dialog_info_item h2, .dialog_info_item span {
+        font-size: 14px;
+        color: white;
+        font-family: 'OpenSans';
+    }
+    .dialog_info_item_img {
+        width: 80px;
+        height: 80px;
+        border-radius: 50%;
+    }
+    .dialog_info_item {
+        display: flex;
+        flex-direction: column;
+        row-gap: 6px;
+        position: relative;
+    }
+    .dialog_info {
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        row-gap: 20px;
+        padding: 20px 25px;
+
+    }
     .dialog_field_header_col span {
         font-size: 12px;
         color: rgba(255, 255, 255, 0.5);
@@ -167,6 +347,8 @@
         font-size: 14px;
         color: rgba(255, 255, 255, 0.5);
         font-family: 'OpenSans';
+        margin-top: -5px;
+        padding: 10px;
     }
     .upload_image {
         width: 20px;
